3f3822d5a03887363bdd6674f13509f7932b8724
Roman Linkov <roman.linkov@vk.com>
2023-02-20 19:00:46 +0300

    Task 27 and 29 is done

---
+++ Task27/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Clear();
+
+int num = NumberInput("Введите число: ");
+
+DigitSumOutput(num);
+
+int SumDigit(int x)
+{
+    int sum=0;
+    int digit=0;
+    while (x > 0)
+    {
+        digit = x % 10;
+        sum=sum+digit;
+        x/=10;
+    }
+    return(sum);
+}
+
+int NumberInput(string msg)
+
+{
+    System.Console.Write(msg);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+void DigitSumOutput(int a)
+{
+    Console.WriteLine($"{a} -> {SumDigit(a)}");
+}
+
---
+++ Task29/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+Console.Clear();
+
+int lngth = NumberInput("Введите длину массива: ");
+int min = NumberInput("Введите минимальный элемент массива: ");
+int max = NumberInput("Введите максимальный элемент массива: ");
+
+int[] nums = ArrayGeneration(lngth, min, max);
+
+Console.Write($" -> [{String.Join(", ", nums)}]");
+Console.WriteLine();
+
+int[] ArrayGeneration(int lngth, int min, int max)
+{
+    Console.WriteLine("Генерируемый массив: ");
+    int[] array = new int[lngth];
+    for (int i = 0; i<lngth;i++)
+    {
+        int value = new Random().Next(min,max+1);
+        array[i] = value;
+        Console.Write($"{value}");
+        if (i<lngth-1) Console.Write($", ");
+    }
+    return array;
+}
+
+int NumberInput(string msg)
+
+{
+    System.Console.Write(msg);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
