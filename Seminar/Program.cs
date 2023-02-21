21adf6ea84398150d8143a66af8c3ff270ee192e
Roman Linkov <roman.linkov@vk.com>
2023-02-21 22:33:32 +0300

    Добавил задачи с семинара

---
+++ Seminar/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Clear();
+
+int num = NumberInput("Введите число: ");
+
+int factorial = Factorial(num);
+
+OutputResult("Факториал введённого числа: ", factorial);
+
+void OutputResult(string msg, int b)
+{
+    Console.WriteLine($"{msg} {b}");
+}
+
+int Factorial(int a)
+{
+    int result=1;
+    int i=1;
+    checked;
+    while (i<a)
+    {
+        i++;
+        result = result*i;
+       // Console.WriteLine(result);
+    }
+    return result;
+}
+
+int NumberInput(string msg)
+{
+    System.Console.Write(msg);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
